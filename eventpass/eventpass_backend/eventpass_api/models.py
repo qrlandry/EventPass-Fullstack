22c25f2f647653816ddc605d0b1fe8c54797f782
Jason Bundy <jason.bundy@me.com>
2023-03-31 11:05:43 -0400

    adding models, working on serializers, etc

--- eventpass/eventpass_backend/eventpass_api/models.py
+++ eventpass/eventpass_backend/eventpass_api/models.py
@@ -1,6 +1,45 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
+class Venue(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=150)
+    zip_code = models.IntegerField(15)
+    capacity = models.IntegerField()
+    indoor = models.BooleanField()
+    outdoor = models.BooleanField()
+
+    def __str__(self):
+        return self.name
+
+
+class Event(models.Model):
+    venue = models.ForeignKey(
+        Venue, on_delete=models.CASCADE, related_name='events')
+    name = models.CharField(max_length=500)
+    datetime = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+
+
+class Customer(models.Model):
+    event = models.ForeignKey(
+        Event, on_delete=models.CASCADE, related_name='customers')
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=150)
+    zip_code = models.IntegerField(15)
+    purchases = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
+
 class User(AbstractUser):
     name = models.CharField(max_length=150)
     email = models.EmailField(max_length=150, unique=True)
