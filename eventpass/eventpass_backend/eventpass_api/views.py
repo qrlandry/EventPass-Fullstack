f70297cc5b3fd795bef1af2f8e781b3c3f74b7b1
Jason Bundy <jason.bundy@me.com>
2023-03-31 11:40:41 -0400

    code cleanup

--- eventpass/eventpass_backend/eventpass_api/views.py
+++ eventpass/eventpass_backend/eventpass_api/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from .serializer import UserSerializer, VenueSerializer, EventSerializer, CustomerSerializer
 from rest_framework.response import Response
 from rest_framework import generics
+from .serializer import UserSerializer, VenueSerializer, EventSerializer, CustomerSerializer
 from .models import User, Venue, Event, Customer
 import jwt
 import datetime
