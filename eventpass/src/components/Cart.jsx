1649f5f238e57034fa3e84c018f25bf3641de425
Michael <michael.s.ross10@gmail.com>
2023-04-02 20:52:27 -0600

    updated cart component to handle discounts and option to remove tickets from cart

--- eventpass/src/components/Cart.jsx
+++ eventpass/src/components/Cart.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom'
 import '../styles/Cart.css'
 
 export default function Cart(){
-  const examples = [
+  const [examples, setExamples] = useState([
     {
       event: "Some Band",
       venue: "Raymond James Stadium",
@@ -20,21 +20,50 @@ export default function Cart(){
       quantity: 1,
       url: "https://media.npr.org/assets/img/2017/12/21/_harlan_nprmusic10years-149_wide-78a7596027f7b4c023a21ad85f078e8a4165c230-s800-c85.jpg"
     }
+  ])
+
+  const validPromoCodes = [
+    'spring15', 'crowdsurf10', 'welcome15', 'employee25'
   ]
   //usecontext for tickets added to cart
   const [ total, setTotal ] = useState(0)
+  const [ totalDiscounted, setTotalDiscounted ] = useState(0)
+  const [ discountAttempt, setDiscountAttempt ] = useState('')
+  const [ isDiscounted, setIsDiscounted ] = useState(false)
+  const [ invalidCode, setInvalidCode ] = useState(false)
+  const [ discountApplied, setDiscountApplied ] = useState(null)
 
-  //need to fix this 
   const calculateTotal = () => {
-    examples.forEach((example) => {
-      setTotal(total + (example['price'] * example['quantity']))
-    })
+    let totalPrice = 0
+    for(let i = 0; i < examples.length; i++){
+    totalPrice += (examples[i]['price']) * Number(examples[i]['quantity'])
+    }
+    setTotal(totalPrice)
+    isDiscounted ? setTotalDiscounted((totalPrice * (100 - discountApplied)/100).toFixed(2)) : 
+    setTotalDiscounted(totalPrice)
   }
 
   useEffect(() => {
     calculateTotal()
-  },[])
+  },[examples, isDiscounted])
 
+  const applyDiscount = () => {
+    if(validPromoCodes.includes(discountAttempt)){
+      setDiscountApplied(parseInt(discountAttempt.slice(-2)))
+      setIsDiscounted(true)
+      setInvalidCode(false)
+      setDiscountAttempt("")
+    }else{
+      setInvalidCode(true)
+      setIsDiscounted(false)
+    }
+  }
+
+
+  //need to convert this into a crUd: update function to remove tickets from user's tickets table
+  const handleCancel = (index) => {
+    setExamples(examples.filter((_,i) => i !== index))
+  }
 
   return (
     <div className="cart-container">
@@ -42,7 +71,7 @@ export default function Cart(){
       <div className="my-bag">
         <h4>My Tickets</h4>
         {
-          examples.map((example) => {
+          examples.map((example,index) => {
             return(
               <div className="cart-item">
                 <img src={example.url} alt={example.event} style={{width: '10vw', height: '10vh', marginRight: '1rem'}}/>
@@ -52,26 +81,40 @@ export default function Cart(){
                   <p className="item-details">{example.date}</p>
                   <p className="item-details">Quantity: {example.quantity}</p>
                 </div>
+                <div className='item-cancel' key={index} onClick={()=> handleCancel(index)}>✕</div>
               </div>
             )
           })
         }
-        <NavLink to="/" className="menu-item" style={{color: 'black'}}>continue shopping</NavLink>
+        <NavLink to="/" className="menu-item" style={{color: 'black'}}>← continue shopping</NavLink>
       </div>
 
 
       <div className="total">
         <h4>ORDER SUMMARY</h4>
         <div className="total-form">
-
           <h5 style={{justifyContent: 'space-between'}}>subtotal ${total}</h5>
-          <h5>fees NONE</h5>
-          <h5>ORDER TOTAL ${total}</h5>
+          <h5>fees $0</h5>
+          {
+            isDiscounted ? 
+            <div className="discounted">
+              <h5>Discount {discountApplied}%</h5>
+              <h5>ORDER TOTAL ${totalDiscounted}</h5>
+            </div> : <h5>ORDER TOTAL ${total}</h5>
+          }
           
         </div>
-        <p style={{fontSize: '1.2vh'}}>Do you have a promo code?</p>
-        <input type="text" className="discount" id="discount-input"/><button className='discount' id="discount-button">Apply</button>
+        <p style={{fontSize: '1.2vh'}}>Have a promo code?</p>
+        <input type="text" className="discount" id="discount-input" value={discountAttempt} onInput={e => setDiscountAttempt(e.target.value)}/><button className='discount' id="discount-button" onClick={applyDiscount}>Apply</button>
+        {
+          invalidCode ? 
+          <div className="invalid-code">
+            <p style={{color: 'red', fontSize: '1vh', marginTop: '0'}}>Please enter a valid code</p>
+          </div> : null
+        }
+    
         {/* need to create handlesubmit function to redirect to checkout component for stripe */}
+        {/* take total and total discounted state to checkout page */}
         <button id="checkout-button">PROCEED TO CHECKOUT</button>
       </div>
 
--- eventpass/src/components/Home.jsx
+++ eventpass/src/components/Home.jsx
@@ -7,7 +7,7 @@ export default function Home(){
     <div className="home">
       <Nav />
       <SearchBar />
-      <h2 className="home-display">Featured Events of the Week</h2>
+      <h2 className="home-display">Weekly Features</h2>
       <div className="third">
         <div className="featured-event">
           <img src="https://acrisurearena.com/wp-content/uploads/2023/03/Static_Digital-Ticketmaster_1920Wx1080H_ODESZA_2023_Regional_AcrisureArena_0920.jpeg" alt="odesza" className="featured-image" style={{width: '100%'}}/>
@@ -45,54 +45,88 @@ export default function Home(){
         </div>
 
       </div>
-      <h2 className="home-display" style={{marginTop: '0'}}>Tonight!</h2>  
+      <h2 className="home-display" style={{marginTop: '0'}}>Nearly Sold Out</h2>  
       <div className="third" id="middle">
+
         <div className="tonight-item">
           <img src="https://koreajoongangdaily.joins.com/data/photo/2023/01/09/a42a47d0-6e6a-4605-9464-717e2c121bfd.jpg" alt="blackpink" className="tonight-image" />
         </div>
+
         <div className="tonight-item">
           <img src="https://static.clubs.nfl.com/image/private/f_auto/bears/pinyquofshcjrbbhjwz2" alt="blackpink" className="tonight-image" />
         </div>
+
         <div className="tonight-item">
           <img src="https://www.cultr.com/wp-content/uploads/2022/10/Blink.jpg" alt="blackpink" className="tonight-image" />
         </div>
+
         <div className="tonight-item">
           <img src="https://i.ebayimg.com/images/g/5DgAAOSwFRxeXUM5/s-l1600.jpg" alt="blackpink" className="tonight-image" />
         </div>
+
         <div className="tonight-item">
           <img src="https://pbs.twimg.com/media/FjUnt4xXEB0gntT?format=jpg&name=large" alt="blackpink" className="tonight-image" />
         </div>
+
         <div className="tonight-item">
           <img src="https://www.summitathletics.com/assets/img/our-work/portfolio/graphic-design/2021-UNH-FB-Poster.jpg" alt="blackpink" className="tonight-image" />
         </div>
+
         <div className="tonight-item">
           <img src="https://media.pitchfork.com/photos/6384e30dc121519c514b9943/master/w_1280%2Cc_limit/The-Weeknd-2023-Tour.jpg" alt="blackpink" className="tonight-image" />
         </div>
+
+        <div className="tonight-item">
+          <img src="https://pbs.twimg.com/media/FjUnt4xXEB0gntT?format=jpg&name=large" alt="blackpink" className="tonight-image" />
+        </div>
+
+        <div className="tonight-item">
+          <img src="https://www.summitathletics.com/assets/img/our-work/portfolio/graphic-design/2021-UNH-FB-Poster.jpg" alt="blackpink" className="tonight-image" />
+        </div>
+
+        <div className="tonight-item">
+          <img src="https://media.pitchfork.com/photos/6384e30dc121519c514b9943/master/w_1280%2Cc_limit/The-Weeknd-2023-Tour.jpg" alt="blackpink" className="tonight-image" />
+        </div>
+
       </div>
 
-      <h2 className="home-display">Need a good laugh?</h2> 
+      <h2 className="home-display">Comedy</h2> 
       <div className="third" id='bottom'>
         <div className="bottom-items">
+
           <div className="bottom-item">
             <img src="https://pbs.twimg.com/media/C5bAlDLWcAAOqnC.jpg" alt="ricky gervais" className="bottom-image" />
           </div>
+
           <div className="bottom-item">
-            <img src="https://m.media-amazon.com/images/M/MV5BNzlmMmFkZTMtZDg3Zi00ZjJmLWJiNzEtZTk0ZWJiZjVkMmRjXkEyXkFqcGdeQXVyMTMzNzIyNDc1._V1_.jpg" alt="ricky gervais" className="bottom-image" />
+            <img src="https://m.media-amazon.com/images/M/MV5BNzlmMmFkZTMtZDg3Zi00ZjJmLWJiNzEtZTk0ZWJiZjVkMmRjXkEyXkFqcGdeQXVyMTMzNzIyNDc1._V1_.jpg" alt="chris rock" className="bottom-image" />
           </div>
+
           <div className="bottom-item">
-            <img src="https://www.propeller.la/cdn-cgi/image/fit=scale-down,width=1000,format=auto,quality=40/files/taylor_tomlinson-propeller-poster-v2_19660.jpg" alt="ricky gervais" className="bottom-image" />
+            <img src="https://www.propeller.la/cdn-cgi/image/fit=scale-down,width=1000,format=auto,quality=40/files/taylor_tomlinson-propeller-poster-v2_19660.jpg" alt="taylor tomlinson" className="bottom-image" />
           </div>
+
           <div className="bottom-item">
-            <img src="http://www.maconcentreplex.org/wp-content/uploads/2022/11/RC2023_Macon_Website-Featured_426x640.jpg" alt="ricky gervais" className="bottom-image" />
+            <img src="http://www.maconcentreplex.org/wp-content/uploads/2022/11/RC2023_Macon_Website-Featured_426x640.jpg" alt="royal comedy" className="bottom-image" />
           </div>
+
           <div className="bottom-item">
-          <img src="https://images.discovery-prod.axs.com/2023/02/uploadedimage_63fccfdf632fd.jpg" alt="kevin hart" className="bottom-featured" />
-        </div>
+            <img src="https://images.discovery-prod.axs.com/2023/02/uploadedimage_63fccfdf632fd.jpg" alt="kevin hart" className="bottom-image" />
+          </div>
+
+          <div className="bottom-item">
+            <img src="https://m.media-amazon.com/images/M/MV5BNzlmMmFkZTMtZDg3Zi00ZjJmLWJiNzEtZTk0ZWJiZjVkMmRjXkEyXkFqcGdeQXVyMTMzNzIyNDc1._V1_.jpg" alt="chris rock" className="bottom-image" />
+          </div>
+
+          <div className="bottom-item">
+            <img src="https://www.propeller.la/cdn-cgi/image/fit=scale-down,width=1000,format=auto,quality=40/files/taylor_tomlinson-propeller-poster-v2_19660.jpg" alt="taylor tomlinson" className="bottom-image" />
+          </div>
+
         </div>
 
       </div>
-      <h2 className="home-display" style={{textAlign: 'center', marginBottom: '1vh'}}>Only a few seats left!</h2> 
-      <img src="https://i.ytimg.com/vi/cJAObWRmWvM/maxresdefault.jpg" alt="" />
+      <h2 className="home-display" style={{textAlign: 'center', marginBottom: '1vh', marginTop: '0'}}>Coming Soon</h2> 
+      <img src="https://i.ytimg.com/vi/cJAObWRmWvM/maxresdefault.jpg" alt="" style={{width: '80vw', marginBottom: '100px'}}/>
 
     </div>
 
