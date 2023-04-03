b2f437cfb5962721670baaea94e6241162a68330
qrlandry <qrlandry@gmail.com>
2023-04-03 09:35:46 -0400

    ticket model

--- eventpass/eventpass_backend/eventpass_api/admin.py
+++ eventpass/eventpass_backend/eventpass_api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import User, Venue, Event, Customer, Ticket
+admin.site.register(User)
+admin.site.register(Venue)
+admin.site.register(Event)
+admin.site.register(Ticket)
+admin.site.register(Customer)
 # Register your models here.
