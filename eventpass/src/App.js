dbafec0dbb156def0936dba1dfb4375ce29feead
Jason Bundy <jason.bundy@me.com>
2023-04-04 12:25:52 -0400

    Updated App.js with axios calls Updated Home.jsx Updated EventDetails.jsx

--- eventpass/src/App.js
+++ eventpass/src/App.js
@@ -9,96 +9,99 @@ import Cart from "./components/Cart";
 import Profile from "./components/Profile";
 import EventDetails from "./components/EventDetails";
 import { CheckSession } from "./services/Auth";
-import axios from "axios";
+import Client from "./services/api";
+import { BASE_URL } from "./services/api";
 
 function App() {
-
-  const [loggedIn, setLoggedIn] = useState(localStorage.getItem("loggedIn") === "true");
+  const [loggedIn, setLoggedIn] = useState(
+    localStorage.getItem("loggedIn") === "true"
+  );
   const [user, setUser] = useState(null);
-  
+  const [evts, setEvents] = useState(null);
+  const [venues, setVenues] = useState(null);
+  const [tickets, setTickets] = useState(null);
+
   const handleLogOut = () => {
-    localStorage.removeItem('jwt');
-    localStorage.removeItem('loggedIn');
+    localStorage.removeItem("jwt");
     setLoggedIn(false);
     setUser(null);
   };
 
   useEffect(() => {
-    const checkLoggedIn = async () => {
-      try {
-        const token = Cookies.get('jwt');
-        if (token) {
-          const res = await axios.get('/api/user', {
-            headers: {
-              'Authorization': `Bearer ${token}`
-            }
-          });
-          setUser(res.data);
-          setLoggedIn(true);
-          localStorage.setItem('jwt', token); // set JWT in local storage
-          localStorage.setItem('loggedIn', 'true');
-        } else {
-          localStorage.removeItem('jwt'); // remove JWT from local storage
-          localStorage.removeItem('loggedIn');
-          setLoggedIn(false);
-          setUser(null);
-        }
-      } catch (error) {
-        console.log(error);
-        localStorage.removeItem('jwt'); // remove JWT from local storage
-        localStorage.removeItem('loggedIn');
-        setLoggedIn(false);
-        setUser(null);
-      }
+    const getSession = async () => {
+      const sessionUser = await CheckSession();
+      setUser(sessionUser);
+      setLoggedIn(sessionUser !== null);
     };
-  
-    const storedUser = localStorage.getItem('user');
-    if (storedUser) {
-      setUser(JSON.parse(storedUser));
-      setLoggedIn(localStorage.getItem('loggedIn') === 'true');
-    } else {
-      checkLoggedIn();
-    }
+
+    getSession();
   }, []);
 
   useEffect(() => {
     console.log("THE USER IS ", user);
-    if (!user) {
-      localStorage.removeItem('jwt');
-      localStorage.removeItem('loggedIn');
-      localStorage.removeItem('user'); // remove user data from local storage
-      setLoggedIn(false);
-    } else {
-      localStorage.setItem('loggedIn', 'true');
-      localStorage.setItem('user', JSON.stringify(user)); // store user data in local storage
-    }
   }, [user]);
+
+  useEffect(() => {
+    const getEvents = async () => {
+      const response = await Client.get(`${BASE_URL}/events`);
+      console.log("EVENTS RETURNED:", response.data);
+      setEvents(response.data);
+    };
+    getEvents();
+  }, []);
+
+  useEffect(() => {
+    const getVenues = async () => {
+      const response = await Client.get(`${BASE_URL}/venues`);
+      console.log("VENUES RETURNED:", response.data);
+      setVenues(response.data);
+    };
+    getVenues();
+  }, []);
+
+  useEffect(() => {
+    const getTickets = async () => {
+      const response = await Client.get(`${BASE_URL}/tickets`);
+      console.log("TICKETS RETURNED:", response.data);
+      setTickets(response.data);
+    };
+    getTickets();
+  }, []);
 
   return (
     <div className="App">
-      <UserContext.Provider value=
-      {{ loggedIn,
-         setLoggedIn,
-         user,
-         setUser,
-         handleLogOut, }}>
-
-      <main>
-        <Routes>
-          <Route path='signin' element={<Login />}/>
-          <Route path='register' element={<Register />}/>
-          <Route path='/' element={<Home />}/>
-            <Route path='/cart' element={<Cart />}/>
-            <Route path='/profile' element={<Profile />}/>
-        </Routes>
-      </main>
-      <footer>
-        <div className="footer">
-          <p>By continuing past this page, you agree to our <a href="#" className="footer-link">Terms of Use.</a></p>
-          <p><a href="#" className="footer-link">Privacy Policy</a> | © 2023 EventPass. All rights reserved.</p>
-          
-        </div>
-      </footer>
+      <UserContext.Provider
+        value={{ loggedIn, setLoggedIn, user, setUser, handleLogOut }}
+      >
+        <main>
+          <Routes>
+            <Route path="signin" element={<Login />} />
+            <Route path="register" element={<Register />} />
+            <Route path="/" element={<Home evts={evts} />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route
+              path="/event/details/:id"
+              element={<EventDetails evts={evts} />}
+            />
+          </Routes>
+        </main>
+        <footer>
+          <div className="footer">
+            <p>
+              By continuing past this page, you agree to our{" "}
+              <a href="#" className="footer-link">
+                Terms of Use.
+              </a>
+            </p>
+            <p>
+              <a href="#" className="footer-link">
+                Privacy Policy
+              </a>{" "}
+              | © 2023 EventPass. All rights reserved.
+            </p>
+          </div>
+        </footer>
       </UserContext.Provider>
     </div>
   );
--- eventpass/src/components/Home.jsx
+++ eventpass/src/components/Home.jsx
@@ -1,70 +1,52 @@
 import Nav from "./Nav";
 import SearchBar from "./SearchBar";
 import "../styles/Home.css";
-import axios from "axios";
-import { useState, useEffect } from "react";
-import { BASE_URL } from "../services/api";
 import { useNavigate } from "react-router-dom";
-import { NavLink } from 'react-router-dom'
 
-export default function Home() {
-  let navigate = useNavigate()
+export default function Home({ evts }) {
+  let navigate = useNavigate();
 
-  const showEvent = (event) => {
-    navigate(`${event.id}`)
-  }
-
-  const [events, setEvents] = useState(null);
-
-  useEffect(() => {
-    const getEvents = async () => {
-      const response = await axios.get(`${BASE_URL}/events`);
-      console.log("EVENTS RETURNED:", response.data);
-      setEvents(response.data);
-    };
-    getEvents();
-  }, []);
+  const showEvent = (id) => {
+    navigate(`event/details/${id}`);
+  };
 
   return (
     <div className="home">
       <Nav />
       <SearchBar />
       <h2 className="home-display">Headliners of the Week</h2>
-      {events ? (
+      {evts ? (
         <div className="headliners">
           <div className="headliner-main-container">
-        
             <img
-              src={events[9]["photo_url"]}
+              src={evts[9]["photo_url"]}
               alt="odesza"
               className="home-image"
-              key={events[9].id}
-              onClick={()=>showEvent(events[9])}
+              key={evts[9].id}
+              onClick={() => showEvent(evts[9].id)}
             />
-    
           </div>
           <div className="headliners-container">
             <img
-              src={events[21]["photo_url"]}
+              src={evts[21]["photo_url"]}
               alt="arctic monkeys"
               className="home-image"
-              key={events[21].id}
-              onClick={()=>showEvent(events[21])}
+              key={evts[21].id}
+              onClick={() => showEvent(evts[21].id)}
             />
           </div>
         </div>
       ) : null}
 
-
       <h2 className="home-display">Sports Tonight</h2>
       <div className="sports-container">
-        {events?.map((event, index) => {
-          if (event["category"] === "sports") {
+        {evts?.map((evt, index) => {
+          if (evt["category"] === "sports") {
             return (
               <div className="sport" key={index}>
                 <img
-                  src={event["photo_url"]}
-                  alt={event["name"]}
+                  src={evt["photo_url"]}
+                  alt={evt["name"]}
                   style={{ maxHeight: "200px" }}
                 />
               </div>
@@ -75,31 +57,34 @@ export default function Home() {
         })}
       </div>
 
-
       <h2 className="home-display">Upcoming Shows</h2>
       <div className="upcoming-container">
-      {
-        events?.map((event, index) => {
-          if(event["category"] === "music" && (event['name'] != 'Odesza' || event['name'] != 'Arctic Monkeys'|| event['name'] != 'Coachella')) {
+        {evts?.map((evt, index) => {
+          if (
+            evt["category"] === "music" &&
+            (evt["name"] != "Odesza" ||
+              evt["name"] != "Arctic Monkeys" ||
+              evt["name"] != "Coachella")
+          ) {
             return (
               <div className="music" key={index}>
-                <img 
-                  src={event["photo_url"]} 
-                  alt={event["name"]}
-                  style={{maxHeight: "200px"}} />
+                <img
+                  src={evt["photo_url"]}
+                  alt={evt["name"]}
+                  style={{ maxHeight: "200px" }}
+                />
               </div>
-            )
+            );
           } else {
             return null;
           }
-        })
-      }
+        })}
       </div>
 
       <h2 className="home-display">Sold Out</h2>
-      {events ? (
+      {evts ? (
         <img
-          src={events[25]["photo_url"]}
+          src={evts[25]["photo_url"]}
           alt="coachella"
           style={{ width: "60%" }}
           className="home-image"
