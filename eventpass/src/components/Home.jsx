d1be01c2619186926a8079c2daf4484f77263a18
Michael <michael.s.ross10@gmail.com>
2023-04-05 14:25:51 -0600

    redesigned homepage and fixed bugs

--- eventpass/src/components/Home.jsx
+++ eventpass/src/components/Home.jsx
@@ -15,7 +15,7 @@ export default function Home({ evts }) {
       <Nav evts={evts}/>
       {/* <SearchBar evts={evts}/> */}
       <div className="headliners-container">
-        <h2 className="home-display">Headliners of the Week</h2>
+        <h2 className="home-display" style={{marginBottom: '2vh', alignItems: 'center'}}>Headliners of the Week</h2>
         {evts ? (
           <div className="headliners">
             <div className="headliner-item">
@@ -26,7 +26,7 @@ export default function Home({ evts }) {
                 key={evts[9].id}
               />
               <div className="headliner-info">
-                <h3>{evts[9].name}</h3>
+                <h3 style={{margin: '0'}}>{evts[9].name}</h3>
                 <button onClick={() => showEvent(evts[9].id)} className="ticket-button">See Tickets</button>
               </div>
             </div>
@@ -38,39 +38,30 @@ export default function Home({ evts }) {
                 key={evts[10].id}
               />
               <div className="headliner-info">
-                <h3>{evts[10].name}</h3>
+                <h3 style={{margin: '0'}}>{evts[10].name}</h3>
               <button onClick={() => showEvent(evts[10].id)} className="ticket-button">See Tickets</button>
+              </div>
+            </div>
+            <div className="headliner-item">
+              <img
+                src={evts[11]["photo_url"]}
+                alt="acdc"
+                className="headliner-image"
+                key={evts[11].id}
+              />
+              <div className="headliner-info">
+                <h3 style={{margin: '0'}}>{evts[11].name}</h3>
+              <button onClick={() => showEvent(evts[11].id)} className="ticket-button">See Tickets</button>
               </div>
             </div>
           </div>
         ) : null}
       </div>
       
-      <div className="sports-container">
-        <h2 className="home-display">Sports</h2>
-        <div className="sports">
-          {evts?.map((evt, index) => {
-            if (evt["category"] === "sports") {
-              return (
-                <div className="sport" key={index}>
-                  <img
-                    src={evt["photo_url"]}
-                    alt={evt["name"]}
-                    style={{ maxHeight: "200px" }}
-                    onClick={() => showEvent(evt.id)}
-                    className="home-image"
-                  />
-                </div>
-              );
-            } else {
-              return null;
-            }
-          })}
-        </div>
-      </div>
+
 
       <div className="upcoming-shows-container">
-        <h2 className="home-display">All Concerts</h2>
+        <h2 className="home-display" style={{marginTop: '0',  marginBottom: '2vh'}}>All Concerts</h2>
         <div className="upcoming-container">
           {evts?.map((evt, index) => {
             if (
@@ -96,7 +87,7 @@ export default function Home({ evts }) {
       </div>
 
       <div className="comedy-container">
-        <h2 className="home-display">Comedy</h2>
+        <h2 className="home-display" style={{marginTop: '5vh', marginBottom: '0'}}>Comedy</h2>
         <div className="comedy">
           {
             evts?.map((evt, index) => {
@@ -108,7 +99,7 @@ export default function Home({ evts }) {
                     <img
                       src={evt["photo_url"]}
                       alt={evt["name"]}
-                      style={{ width: "100%", marginTop: "2vh" }}
+                      style={{ width: "100%", marginTop: "2vh", border: '0.2px solid gray' }}
                       onClick={() => showEvent(evt.id)}
                       className="home-image"
                     />
@@ -124,13 +115,36 @@ export default function Home({ evts }) {
         </div>
       </div>
 
+      <div className="sports-container">
+        <h2 className="home-display" style={{marginTop: '5vh',  marginBottom: '2vh'}}>Sports</h2>
+        <div className="sports">
+          {evts?.map((evt, index) => {
+            if (evt["category"] === "sports") {
+              return (
+                <div className="sport" key={index}>
+                  <img
+                    src={evt["photo_url"]}
+                    alt={evt["name"]}
+                    style={{ maxHeight: "200px" }}
+                    onClick={() => showEvent(evt.id)}
+                    className="home-image"
+                  />
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </div>
+      </div>
+
       <div className="soldout-container">
-        <h2 className="home-display">Sold Out</h2>
+        <h2 className="home-display" style={{marginTop: '5vh'}}>Sold Out</h2>
         {evts ? (
           <img
             src={evts[25]["photo_url"]}
             alt="coachella"
-            style={{ width: "60%", marginBottom: '100px' }}
+            style={{ width: "100%", marginBottom: '100px', marginTop: '2vh' }}
             className="home-image"
             onClick={() => showEvent(evts[25].id)}
           />
